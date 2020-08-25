a26834ad2cec05320069941eabe355d9c095e1a2
Aldy Nur Pratama <aldypratama96@gmail.com>
2020-08-25 16:11:38 +0700

    Django URL w Parameters

---
+++ Novice/02-02/listkerja/list/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+
+from . import models
+
+def index(req):
+    if req.POST:
+        print(req.POST)
+        models.Tugas.objects.create(Task=req.POST['name'])
+
+    data = models.Tugas.objects.all()
+    return render(req, 'list/index.html', {
+        'data': data,
+    })
+
+def detail(req, id):
+    data = models.Tugas.objects.filter(pk=id).first()
+    return render(req, 'list/detail.html', {
+        'data' : data,
+    })
