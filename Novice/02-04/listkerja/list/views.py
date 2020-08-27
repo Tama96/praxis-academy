047feff4877d449e26eedb0f3c9f75218ae1e66d
Aldy Nur Pratama <aldypratama96@gmail.com>
2020-08-27 12:12:55 +0700

    Edit Button Function

---
+++ Novice/02-04/listkerja/list/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+
+
+
+from . import models
+
+def index(req):
+    if req.POST:
+        print(req.POST)
+        models.Tugas.objects.create(Task=req.POST['name'])
+
+    data = models.Tugas.objects.all()
+    return render(req, 'list/index.html', {
+        'data': data,
+    })
+
+def detail(req, id):
+    data = models.Tugas.objects.filter(pk=id).first()
+    return render(req, 'list/detail.html', {
+        'data' : data,
+    })
+
+def delete(req, id):
+    models.Tugas.objects.filter(pk=id).delete()
+    return redirect('/')
+
+def edit(req, id):
+    if req.POST:
+        data = models.Tugas.objects.filter(pk=id).update(Task=req.POST['name'])
+        return redirect('/')
+    data = models.Tugas.objects.filter(pk=id).first()
+    return render(req, 'list/edit.html', {
+        'data': data,
+    })
