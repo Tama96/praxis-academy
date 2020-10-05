8255046f9ed9d06e3e787c7a12b85c8c87fb5673
Tama96 <jackalopes.460467@gmail.com>
2020-10-05 14:43:12 +0700

    membuat JSON

--- Novice/08-01/Django_REST_API/task/views.py
+++ Novice/08-01/Django_REST_API/task/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
+
+def index(request):
+    comments = [
+        {'nama' : 'Joko Kendil',
+        'motto' : 'Royco'},
+        {'nama' : 'Surya Piningit',
+        'motto' : 'Masako'}
+    ]
+
+    return JsonResponse(comments, safe=False)
